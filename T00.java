2597c8f6b1310f9b061aaef32bd7a7b96e204044
immanuelsimaremare-max <immanuelsimaremare@gmail.com>
2025-10-03 20:43:30 +0700

    PAIR 10
     SELESAI

--- T00.java
+++ T00.java
@@ -7,42 +7,42 @@ public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a, b, t, k;
+        int a, b, tambah, kali;
 
         a = Integer.parseInt(input.nextLine());
         b = Integer.parseInt(input.nextLine());
         if (a % 2 == 0) {
-            System.out.println("Bilangan Pertama adalah Genap");
+            System.out.println("Bilangan pertama: Genap");
         } else {
-            System.out.println("Bilangan Pertama adalah Ganjil");
+            System.out.println("Bilangan pertama: Ganjil");
         }
         if (b % 2 == 0) {
-            System.out.println("bilangan kedua genap");
+            System.out.println("Bilangan kedua: Genap");
         } else {
-            System.out.println("bilangan kedua ganjil");
+            System.out.println("Bilangan kedua: Ganjil");
         }
         if (a > b) {
-            System.out.println(" KEDUA BILANGAN SAMA BESAR");
+            System.out.println("Bilangan pertama lebih besar");
         } else {
             if (a < b) {
-                System.out.println(" BIL PERTAMA LEBIH BESAR");
+                System.out.println("Bilangan kedua lebih besar");
             } else {
-                System.out.println("BIL PERTAMA LEBIH KECIL");
+                System.out.println("Kedua bilangan sama besar");
             }
         }
         if (a % 2 == 0) {
             if (b % 2 == 0) {
-                t = a + b;
-                System.out.println("HASIL PENJUMLAHAN: " + t);
+                tambah = a + b;
+                System.out.println("Hasil penjumlahan: " + tambah);
             } else {
-                System.out.println("BERBEDA JENIS");
+                System.out.println("Berbeda jenis");
             }
         } else {
             if (b % 2 != 0) {
-                k = a * b;
-                System.out.println("HASIL PERKALIAN: " + k);
+                kali = a * b;
+                System.out.println("Hasil perkalian: " + kali);
             } else {
-                System.out.println("BERBEDA JENIS");
+                System.out.println("Berbeda jenis");
             }
         }
     }
